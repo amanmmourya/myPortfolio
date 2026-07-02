6ba145ae31580e316bc0b3b9e58a60b9379aae90
Mourya Aman Mahendra <mouryaaman69@gmail.com>
2026-07-02 23:23:00 +0530

    Add new skills to skillSet array
    Updated skillSet array to include additional skills such as SpringBoot, Kafka, Redis, and Docker.

--- components/about.js
+++ components/about.js
@@ -12,7 +12,7 @@ const about = () => {
         setEdu(false);
         setSkill(true);
     }
-    const skillSet = ["Java", "C++", "Python","R","HTML","CSS","Tailwind CSS", "JavaScript", "MongoDB","MySQL", "Node.js", "Express.js","socket.io","Mongoose ODM", "React", "Next.js","OpenCV","YOLO","Pyserial","Arduino IDE", "Numpy", "Matplotlib","Pandas", "LangChain", "LTSpice", "NGSpice", "MatLab","Verilog","Embedded C"]
+    const skillSet = ["Java", "C++", "Python","R","HTML","CSS","Tailwind CSS", "JavaScript", "MongoDB","MySQL", "Node.js", "Express.js","SpringBoot","socket.io","Kafka","Redis","Mongoose ODM", "React", "Next.js","Docker","OpenCV","YOLO","Pyserial","Arduino IDE", "Numpy", "Matplotlib","Pandas", "LangChain", "LTSpice", "NGSpice", "MatLab","Verilog","Embedded C"]
     return (
         <section id='about'>
             <div className='about-container'>
